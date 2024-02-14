c81352b4a44cd3cf62d85a9936421c231d6143f3
dipta ikromi m <dipta.ikromi@unej.ac.id>
2024-02-15 01:26:25 +0700

    test

--- routes/routes.php
+++ routes/routes.php
@@ -4,7 +4,6 @@ use App\Controllers\AuthController;
 use App\Controllers\CommentController;
 use App\Controllers\WelcomeController;
 use App\Middleware\AuthMiddleware;
-use App\Middleware\CorsMiddleware;
 use Core\Routing\Route;
 
 /**
@@ -28,7 +27,7 @@ Route::prefix('/api')->group(function () {
         // Get and create comment
         Route::controller(CommentController::class)->group(function () {
             Route::get('/', 'get');
-            Route::post('/', 'create')->middleware(CorsMiddleware::class); 
+            Route::post('/', 'create');
         });
 
         Route::options('/'); // Preflight request [/api/comment]
