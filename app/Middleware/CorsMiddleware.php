71d97298e3c13d989186d457893dcd4ff5ff5226
dipta ikromi m <dipta.ikromi@unej.ac.id>
2024-02-15 00:26:28 +0700

    test

--- app/Middleware/CorsMiddleware.php
+++ app/Middleware/CorsMiddleware.php
@@ -19,29 +19,29 @@ final class CorsMiddleware implements MiddlewareInterface
         // Set Access-Control-Allow-Origin header to allow all origins
         $header->set('Access-Control-Allow-Origin', '*');
 
-        $vary = (!$header->has('Vary')) ? [] : explode(', ', $header->get('Vary'));
-        $vary = array_unique([...$vary, 'Accept', 'Access-Control-Request-Method', 'Access-Control-Request-Headers', 'Origin', 'User-Agent']);
-        $header->set('Vary', join(', ', $vary));
+        // $vary = (!$header->has('Vary')) ? [] : explode(', ', $header->get('Vary'));
+        // $vary = array_unique([...$vary, 'Accept', 'Access-Control-Request-Method', 'Access-Control-Request-Headers', 'Origin', 'User-Agent']);
+        // $header->set('Vary', join(', ', $vary));
 
-        if (!$request->method(Request::OPTIONS)) {
-            return $next($request);
-        }
+        // if (!$request->method(Request::OPTIONS)) {
+        //     return $next($request);
+        // }
 
-        $header->unset('Content-Type');
+        // $header->unset('Content-Type');
 
-        if (!$request->server->has('HTTP_ACCESS_CONTROL_REQUEST_METHOD')) {
-            return respond()->setCode(Respond::HTTP_NO_CONTENT);
-        }
+        // if (!$request->server->has('HTTP_ACCESS_CONTROL_REQUEST_METHOD')) {
+        //     return respond()->setCode(Respond::HTTP_NO_CONTENT);
+        // }
 
-        $header->set(
-            'Access-Control-Allow-Methods',
-            strtoupper($request->server->get('HTTP_ACCESS_CONTROL_REQUEST_METHOD', $request->method(),'GET,POST,DELETE,PUT,OPTIONS'))
-        );
+        // $header->set(
+        //     'Access-Control-Allow-Methods',
+        //     strtoupper($request->server->get('HTTP_ACCESS_CONTROL_REQUEST_METHOD', $request->method(),'GET,POST,DELETE,PUT,OPTIONS'))
+        // );
 
-        $header->set(
-            'Access-Control-Allow-Headers',
-            $request->server->get('HTTP_ACCESS_CONTROL_REQUEST_HEADERS', 'Accept, Authorization, Content-Type, Origin, Token, User-Agent')
-        );
+        // $header->set(
+        //     'Access-Control-Allow-Headers',
+        //     $request->server->get('HTTP_ACCESS_CONTROL_REQUEST_HEADERS', 'Accept, Authorization, Content-Type, Origin, Token, User-Agent')
+        // );
 
         return respond()->setCode(Respond::HTTP_NO_CONTENT);
     }
